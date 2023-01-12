183794663f65855666091ce7c29fde6c64be0ca3
mattpe <mattpe@metropolia.fi>
2023-01-12 15:30:02 +0200

    Add some input/output examples.

---
+++ moduuli-02/esim.py
@@ -0,0 +1,21 @@
+# print("2")
+# print()
+print('Hei, "maailma!" 3')
+
+# muuttujan esittely ja arvon sijoittaminen muuttujaan
+name = "Matti"
+print(name)
+name = "Viivi"
+print(name)
+
+# syötteen lukeminen käyttäjältä¿
+name = input("Anna nimesi: ")
+ageString = input("Anna ikäsi: ")
+# arvon tyypin muunnos merkkijono (string) => kokonaisluku (int)
+# int("3") => 3
+age = int(ageString)
+age = age + 1
+message = "Morjes " + name + ", olet ensi vuonna " + str(age) + "-vuotias."
+print(message)
+# sama käyttäen f-stringiä
+print(f"Morjes {name}, olet ensi vuonna {age}-vuotias.")
