cca9409e1d3b46bfdb5236bf204e8a5f0f6e08af
jormaraty <jorma.raty@metropolia.fi>
2023-01-16 20:18:19 +0200

    Algoritmi tehtävän ratkaisemiseksi.

---
+++ moduuli-03/teht4.py
@@ -0,0 +1,21 @@
+# Eräs algoritmi tehtävän ratkaisuksi
+# Olisiko meillä jokin kiva valintarakenne tämän toteuttamiseksi?
+
+'''
+JOS vuosi on jaollinen 400:lla, niin
+    vuosi ON karkausvuosi
+MUUTEN JOS vuosi on jaollinen 100:lla, niin
+    vuosi EI OLE karkausvuosi
+MUUTEN JOS vuosi on jaollinen 4:llä, niin
+    vuosi ON karkausvuosi
+MUUTEN
+    vuosi EI OLE karkausvuosi
+'''
+
+'''
+Eri vuosilukuja, joilla voit testata koodiasi:
+2000: ON
+1900: EI ole
+2023: EI ole
+2024: ON
+'''
