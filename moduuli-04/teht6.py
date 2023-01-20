d951a346e5acaf289991465d878f9f50b48af0a6
mattpe <mattpe@metropolia.fi>
2023-01-20 15:07:38 +0200

    add mod4 t6 "starter"

---
+++ moduuli-04/teht6.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+N = 1000000
+counter = 0
+n = 0
+while counter < N:
+    counter += 1
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    # print(f"arvotty piste {x}, {y}")
+    # onko piste yksikköympyrän sisällä: x^2+y^2<1
+    # print(f"{x * x + y * y < 1}")
+    if x * x + y * y < 1:
+        # print("on sisällä")
+        n += 1
+print(f"Pisteitä arvottu yhteensä {counter}, joista {n} kpl ympyrän sisällä")
+# TODO: laske piin likiarvo käyttäen annettua kaavaa
