4382963fcd2bdb09ec9cc24f442ba2515ccfc57c
jormaraty <jorma.raty@metropolia.fi>
2023-02-02 10:10:19 +0200

    First version.

--- moduuli-07/teht3.py
+++ moduuli-07/teht3.py
@@ -5,7 +5,10 @@ hakea jo syötetyn lentoaseman tiedot vai lopettaa.
 Jos käyttäjä valitsee uuden lentoaseman syöttämisen,
 ohjelma kysyy käyttäjältä lentoaseman ICAO-koodin ja nimen.
 Jos käyttäjä valitsee haun, ohjelma kysyy ICAO-koodin ja
-tulostaa sitä vastaavan lentoaseman nimen. Jos käyttäjä haluaa lopettaa, ohjelman suoritus päättyy. Käyttäjä saa valita uuden toiminnon miten monta kertaa tahansa aina siihen asti, kunnes hän haluaa lopettaa. (ICAO-koodi on lentoaseman yksilöivä tunniste. Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK. Löydät koodeja helposti selaimen avulla.)
+tulostaa sitä vastaavan lentoaseman nimen. Jos käyttäjä haluaa lopettaa, ohjelman suoritus päättyy.
+Käyttäjä saa valita uuden toiminnon miten monta kertaa tahansa aina siihen asti, kunnes hän haluaa lopettaa.
+(ICAO-koodi on lentoaseman yksilöivä tunniste. Esimerkiksi Helsinki-Vantaan lentoaseman ICAO-koodi on EFHK.
+Löydät koodeja helposti selaimen avulla.)
 '''
 
 # funktio lisää uuden lentoaseman sanakirjaan
